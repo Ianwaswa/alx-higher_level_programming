2d5edb48f6c43dd2c85fc617f7a15a82e4251959
Ianwaswa <ianwaswa2019@gmail.com>
2024-02-06 12:56:23 -0800

    code update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Module for saving to json"""
-import json
-import os.path
-import sys
-from sys import argv
+"""This module defines a class Student"""
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-json_list = []
+class Student:
+    """Represent a student."""
 
-if os.path.exists(filename):
-    json_list = load_from_json_file(filename)
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-for index in argv[1:]:
-    json_list.append(index)
-
-save_to_json_file(json_list, filename)
+    def to_json(self):
+        """Gets a dictionary representation of the Student"""
+        return self.__dict__
