f6f100a14cf99a62bd68378a5b5be26325ece43c
Ianwaswa <ianwaswa2019@gmail.com>
2024-08-25 21:23:38 -0700

    updatee code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
+import hidden_4
 
+def main():
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    filtered_names = [name for name in names if not name.startswith("__")]
+    for name in sorted(filtered_names):
+        print(name)
+
+if __name__ == "__main__":
+    main()
